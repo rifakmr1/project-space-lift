54e60cc0a3ee10276e7bf5af998083d0c6fe172a
rifakmr1 <rifakathulla456@gmail.com>
2026-01-18 02:00:48 +0530

    updated sku for public ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -102,6 +102,7 @@ resource "azurerm_public_ip" "pip" {
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_interface" "nic" {
