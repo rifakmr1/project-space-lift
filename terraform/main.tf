7fd837916a7649cb3360fbd47183700b952dbdb7
rifakmr1 <rifakathulla456@gmail.com>
2026-01-18 02:27:06 +0530

    updated vault name

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,7 +47,7 @@ resource "azurerm_resource_group" "rg" {
 # Key Vault (AWS SSM equivalent)
 # ----------------------------
 resource "azurerm_key_vault" "kv" {
-  name                       = "demo-kv-${random_string.suffix.result}"
+  name                       = "kv-spacelift-cicd-ic"
   location                   = var.location
   resource_group_name        = azurerm_resource_group.rg.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
